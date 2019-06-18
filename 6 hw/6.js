6ce279b3045d9ff91739ece4ab19328ea75b834e
Anton Yermakovich <antonyermakovich@gmail.com>
2019-06-18 13:00:37 +0300

    6 hw

---
+++ 6 hw/6.js
@@ -0,0 +1,62 @@
+//   Практическое задание 2:
+
+function Cat(name) {
+	var foodAmount = 50;
+
+    this.name = name;
+
+	formatFoodAmount = function() {
+		return foodAmount + ' гр.';
+	};
+
+    this.feed = function() {
+		console.log('Насыпаем в миску ' + formatFoodAmount() + ' корма.');
+    };
+}
+
+var barsik = new Cat('Барсик');
+
+console.log(barsik.name);
+
+// barsik = null;
+
+console.log(Cat.feed);
+
+// ==============================================================
+
+//   Практическое задание 3:
+
+function Cat(name) {
+	var foodAmount = 50;
+
+    this.name = name;
+
+	this.dailyNorm = function (x) {
+		if (x < 50 || x > 500) {
+			console.log('Ошибка...');
+			return;
+		} else if (x == undefined) {
+			return formatFoodAmount();
+		}
+
+		foodAmount = x;
+
+		return formatFoodAmount();
+	};
+
+	formatFoodAmount = function() {
+		return foodAmount + ' гр.';
+	};
+
+    this.feed = function() {
+		console.log('Насыпаем в миску ' + formatFoodAmount() + ' корма.');
+    };
+}
+
+var barsik = new Cat('Барсик');
+
+console.log(barsik.name);
+
+// barsik = null;
+
+console.log(barsik.feed());
